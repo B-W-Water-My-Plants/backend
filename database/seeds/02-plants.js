afbc603a357427451f00cf78acd7fe8c85f8dd41
Danica Valdez <danicaevaldez@gmail.com>
2020-02-29 16:25:09 -0900

    added id to plants seed file

--- database/seeds/02-plants.js
+++ database/seeds/02-plants.js
@@ -4,6 +4,7 @@ exports.seed = function(knex) {
       .then(function() {
         return knex('plants').insert([
           {
+            id: 1,
             nickname: 'Sammy',
             species: "Cactus",
             h2o_frequency: "Monthly",
@@ -11,6 +12,7 @@ exports.seed = function(knex) {
             user_id: 1
           },
           {
+            id: 2,
             nickname: 'Fred',
             species: "Fern",
             h2o_frequency: "Weekly",
